058af8c6435c49bc5a2ee38218b15de4671aa0bf
AliJafarli <somagni4icent@gmail.com>
2024-11-09 03:33:44 +0400

    1

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {8, 2, 4 , 9 , 10 , 11, 12, 13}; // temp 5, arr[j + 1] = temp
+        int[] arr = {8, 2, 4, 9, 10, 11, 12, 13}; // temp 5, arr[j + 1] = temp
         //   2, 2, 8, 1, 6, 2 ,7, 4
 
         System.out.println(Arrays.toString(arr));
 
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             boolean isSorted = false;
-            for(int j = 0; j < arr.length - i- 1; j++){
-                if(arr[j] > arr[j + 1]){
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1]; // 2
                     arr[j + 1] = temp; // 5
@@ -20,10 +20,10 @@ public class BubbleSort {
                 }
                 System.out.println(Arrays.toString(arr));
             }
-            if(!isSorted){
+            if (!isSorted) {
                 break;
             }
-             System.out.println("-------------------");
+            System.out.println("-------------------");
         }
     }
 }
