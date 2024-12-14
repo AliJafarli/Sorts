5042ce5c4514b6b4b8e025b3773522ce327b4d9a
AliJafarli <somagni4icent@gmail.com>
2024-12-14 05:37:04 +0400

    1

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -15,8 +15,8 @@ public class BubbleSort {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1]; // 2
-                    arr[j + 1] = temp; // 5
-                     isSorted = true;
+                     arr[j + 1] = temp; // 5
+                      isSorted = true;
                 }
                 System.out.println(Arrays.toString(arr));
             }
