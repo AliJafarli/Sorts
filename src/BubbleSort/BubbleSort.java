0551856b3de3a9a23458211be1317a04df6bcd35
AliJafarli <somagni4icent@gmail.com>
2025-09-02 09:07:32 +0400

    Sorting

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -10,22 +10,23 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));
 
         for (int i = 0; i < arr.length; i++) {
-            boolean isSorted = false;
+            boolean isSorted = true;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    isSorted = true;
+                    isSorted = false;
                 }
 
             }
 
-            if (!isSorted) {
+            if (isSorted) {
                 break;
             }
             System.out.println("-------------------");
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println(Arrays.toString(arr));
+
     }
 }
